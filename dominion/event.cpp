f2e931400e25da4e2823c5a482f8255549c5a23c
Kyle Hes <hess.kyler@gmail.com>
2020-09-03 13:04:12 -0600

    Added event system

---
+++ dominion/event.cpp
@@ -0,0 +1,9 @@
+//
+//  event.cpp
+//  dominion
+//
+//  Created by Kyle Hess on 8/29/20.
+//  Copyright © 2020 Kyle Hess. All rights reserved.
+//
+
+#include "event.hpp"
---
+++ dominion/event.hpp
@@ -0,0 +1,14 @@
+//
+//  event.hpp
+//  dominion
+//
+//  Created by Kyle Hess on 8/29/20.
+//  Copyright © 2020 Kyle Hess. All rights reserved.
+//
+
+#ifndef event_hpp
+#define event_hpp
+
+#include <stdio.h>
+
+#endif /* event_hpp */
